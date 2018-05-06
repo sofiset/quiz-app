09528680d150052d8de326a95fa8cabe084c9da8
Sophie Fiset <sfiset5@gmail.com>
2018-05-06 16:28:38 -0400

    Forgot to push earlier..

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 import React, { Component } from 'react';
 import './App.css';
-import wasteWizardData from './swm_waste_wizard_APR.json';
+import wasteWizardData from './wasteWizardData.json';
 
 class App extends Component {
 
@@ -11,19 +11,26 @@ class App extends Component {
     this.handleAnswer = this.handleAnswer.bind(this)
     this.initializeQuestions = this.initializeQuestions.bind(this)
     this.checkIsQuizDone = this.checkIsQuizDone.bind(this)
+    this.checkAnswer = this.checkAnswer.bind(this)
+
+    const uniqueAnswerOptions = getAllUniqueAnswerOptions(wasteWizardData)
 
     this.state = {
-      numberOfQuestions: 10,
+
+      numQuestionsPerRound: 10,
+      numOptionsShown: 3,
+      questions: [],
+
+      isQuizDone: false,
+      currentScore: 0,
+
       currentAnswer: null,
-      questions: Array(10).fill(null),
-      values: [1, 2, 3],
-      currentAnswerStatus: null,
-      correctAnswer: 2,
       currentQuestionIndex: 0,
-      isQuizDone: "false"
+      currentAnswerStatus: null
+      
     } 
 
-    this.initializeQuestions()
+    this.initializeQuestions(uniqueAnswerOptions)
 
   }
 
@@ -34,42 +41,81 @@ class App extends Component {
   }
 
   render() {
+
+    const {
+      currentQuestionIndex,
+      questions,
+      isQuizDone,
+      currentAnswerStatus,
+      currentScore
+    } = this.state
+
     return (
       <div>
-        <h1>{this.state.questions[this.state.currentQuestionIndex]}</h1>
-        <h2>{this.state.currentAnswerStatus}</h2>
-        <p>Done ? {this.state.isQuizDone}</p>
-        <Question buttonValues={this.state.values} handleAnswer={this.handleAnswer}/>
+        <h1>{currentQuestionIndex+1}) {questions[currentQuestionIndex].title} </h1>
+        <p>correct answer: { questions[currentQuestionIndex].correct_answer || null }</p>
+        <h2>{currentAnswerStatus}</h2>
+        <p>score: {currentScore}</p>
+        <p>{isQuizDone ? "Done!" : ""}</p>
+        <Question buttonValues={questions[currentQuestionIndex].answer_options} handleAnswer={this.handleAnswer} />
       </div>
     );
   } 
 
-  initializeQuestions() {
+  initializeQuestions(uniqueAnswerOptions) {
 
-    var newQuestions = this.state.questions
-
+    let newQuestions = []
     const keys = Object.keys(wasteWizardData)
+    const possibleAnswerOptions = uniqueAnswerOptions
 
-    newQuestions = newQuestions
-      .map(() => {
-        return wasteWizardData[getRandomIndex(keys)]
+    for (let i = 0; i < this.state.numQuestionsPerRound; i++) {
+
+      let question = wasteWizardData[getRandomIndex(keys)]
+
+      newQuestions.push({
+        "title" : question.TITLE,
+        "correct_answer" : question.DESC_ID,
+        "answer_options" : getAnswerOptionSet(question.DESC_ID, possibleAnswerOptions, this.state.numOptionsShown)
+
+        /*"answer_options" : [
+          question.DESC_ID,
+          wasteWizardData[getRandomIndex(keys)].DESC_ID,
+          wasteWizardData[getRandomIndex(keys)].DESC_ID
+        ]*/
+
       })
-      .map(x => x.title)
+
+    }
 
     this.state.questions = newQuestions
+    // this.setState((state) => ({questions: newQuestions}))
 
   }
 
   handleAnswer(answer) {
 
-    this.setState({ currentAnswer: answer})
-    this.setState({ currentAnswerStatus: (answer == this.state.correctAnswer) ? "true" : "false" })
-    this.setState({ currentQuestionIndex: this.state.currentQuestionIndex+1 })
-    this.setState({ isQuizDone: this.checkIsQuizDone()})
+    if (this.checkIsQuizDone()) {
+      this.setState({isQuizDone: true})
+      return
+    }
+
+    const isAnswerTrue = this.checkAnswer(answer)
+
+    this.setState({ 
+      currentAnswer: answer,
+      currentAnswerStatus: isAnswerTrue,
+      currentScore: isAnswerTrue ? this.state.currentScore+1 : this.state.currentScore,
+      currentQuestionIndex: this.state.currentQuestionIndex+1
+    })
+
   }
 
   checkIsQuizDone() {
-    return this.state.currentQuestionIndex == this.state.numberOfQuestions -1? "true" : "false"
+    return this.state.currentQuestionIndex === this.state.numQuestionsPerRound-1
+  }
+
+  checkAnswer(answer) {
+     return answer === this.state.questions[this.state.currentQuestionIndex].correct_answer
   }
 
 }
@@ -77,13 +123,14 @@ class App extends Component {
 class Question extends Component {
 
   render() {
-    const { buttonValues, handleAnswer, ...rest } = this.props // destructuring
-    const newButtonValues = [...buttonValues, {}] // spreading
+
+    const { buttonValues, handleAnswer } = this.props // destructuring
+
     return (
       <div>
-        <AnswerOption value={this.props.buttonValues[0]} handleAnswer={this.props.handleAnswer}/>
-        <AnswerOption value={this.props.buttonValues[1]} handleAnswer={this.props.handleAnswer}/>
-        <AnswerOption value={this.props.buttonValues[2]} handleAnswer={this.props.handleAnswer}/>
+        <AnswerOption value={buttonValues[0]} handleAnswer={handleAnswer}/>
+        <AnswerOption value={buttonValues[1]} handleAnswer={handleAnswer}/>
+        <AnswerOption value={buttonValues[2]} handleAnswer={handleAnswer}/>
       </div>
     );
   }
@@ -103,6 +150,63 @@ class AnswerOption extends Component {
 function getRandomIndex(arr) {
   // wasteWizardData
   return Math.floor(Math.random() * arr.length)
+}
+
+function getAllUniqueAnswerOptions(data) {
+
+  const items = data
+  var lookup = {} 
+  var uniqueAnswerOptions = []
+
+  for (var item, i = 0; item = items[i++];) {
+
+    var name = item.DESC_ID
+
+    if (!(name in lookup)) {
+      lookup[name] = 1
+      uniqueAnswerOptions.push(name)
+    }
+  }
+
+  return uniqueAnswerOptions
+
+}
+
+function getAnswerOptionSet(correctAnswer, allUniqueOptions, totalNumOptions) {
+
+  const possibleOptions = allUniqueOptions
+  let answerOptionSet = [correctAnswer]
+
+  while (answerOptionSet.length < totalNumOptions) {
+    let index = getRandomIndex(possibleOptions)
+    if (possibleOptions[index] !== correctAnswer) {
+      answerOptionSet.push(possibleOptions[index])
+    }
+  }
+
+  return shuffleOptions(answerOptionSet)
+
+}
+
+function shuffleOptions (originalArray) {
+
+  var array = [].concat(originalArray);
+  var currentIndex = array.length, temporaryValue, randomIndex;
+
+  // While there remain elements to shuffle...
+  while (0 !== currentIndex) {
+
+    // Pick a remaining element...
+    randomIndex = Math.floor(Math.random() * currentIndex);
+    currentIndex -= 1;
+
+    // And swap it with the current element.
+    temporaryValue = array[currentIndex];
+    array[currentIndex] = array[randomIndex];
+    array[randomIndex] = temporaryValue;
+  }
+
+  return array;
 }
 
 export default App;
